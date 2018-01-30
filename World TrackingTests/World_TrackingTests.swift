0649f27284de3783f51dd53c94e4e4b409b26d0f
Alexander Tallqvist <alexander.tallqvist@karhuhelsinki.fi>
2018-01-30 13:46:05 +0200

    Initial Commit

---
+++ World TrackingTests/World_TrackingTests.swift
@@ -0,0 +1,36 @@
+//
+//  World_TrackingTests.swift
+//  World TrackingTests
+//
+//  Created by Alexander Tallqvist on 30/01/2018.
+//  Copyright © 2018 Alexander Tallqvist. All rights reserved.
+//
+
+import XCTest
+@testable import World_Tracking
+
+class World_TrackingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
