8f9cf7ebc1ce9eb3a084602c8c32b69c56c09eac
Alexander Tallqvist <alexander.tallqvist@karhuhelsinki.fi>
2018-03-02 20:10:19 +0200

    Changes to mostly our IkeaViewController

--- World Tracking/IkeaViewController.swift
+++ World Tracking/IkeaViewController.swift
@@ -11,11 +11,12 @@ import ARKit
 
 // Extend UICollectionVIewDataSrouce because of our collection view
 // UICollectionViewDelegate > didSelectItem?
-class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
 
     @IBOutlet weak var itemsCollectionView: UICollectionView!
     @IBOutlet weak var ikeaSceneView: ARSCNView!
-    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    @IBOutlet weak var planeDetected: UILabel!
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table", "shelf"]
     let configuration = ARWorldTrackingConfiguration()
     var selectedItem: String?
     
@@ -28,11 +29,17 @@ class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollec
         self.ikeaSceneView.session.run(configuration)
         
         // Override our extended calsses
-        self.itemsCollectionView.dataSource = self;
-        self.itemsCollectionView.delegate = self;
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
         
         // Register our gesture recognizer
         self.registerGestureRecognizers()
+        
+        // Register our plane detected label function
+        self.ikeaSceneView.delegate = self
+        
+        // Add a lighting effect
+        self.ikeaSceneView.autoenablesDefaultLighting = true;
         // Do any additional setup after loading the view.
     }
 
@@ -43,11 +50,41 @@ class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     // Register user tap gesture (needs to be exposed to objeC
     func registerGestureRecognizers() {
+        // Create the "tap" geasture recognizer
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        // Add our custom tapGeastureRecognizer to the scene view
+        
+        // Create a "pinch" geasture recognizer
+        let pinchGeastureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        
+        // Create a "long press" recognizer
+        let longPressGeastureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
+        
+        // Add our custom tap, picnch and long press recognizers to the scene view
+        self.ikeaSceneView.addGestureRecognizer(pinchGeastureRecognizer)
         self.ikeaSceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.ikeaSceneView.addGestureRecognizer(longPressGeastureRecognizer)
     }
     
+    // Our "pinch" function
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let pinchLocation = sender.location(in: sceneView)
+        
+        // Check if we "pinch" an object
+        let hitTest = sceneView.hitTest(pinchLocation)
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            let node = results.node
+            // Scale the node acording to how "far" we pinched it
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node.runAction(pinchAction)
+            // Reset our scaling value to 1, so that the item that
+            // we're scaling grows at a constant rate, instead of exponentially
+            sender.scale = 1.0
+        }
+    }
+    
+    // Our "tap" function
     @objc func tapped(sender: UITapGestureRecognizer) {
         // Check if the surface we tapped is a horizontal surface
         let sceneView = sender.view as! ARSCNView
@@ -64,9 +101,43 @@ class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollec
         }
     }
     
+    // Our "long press" function
+    @objc func rotate(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+        
+        if !hitTest.isEmpty {
+            let result = hitTest.first!
+            // We are currently "pressing" an object
+            if sender.state == .began {
+                // Apply the a rotation to the selected node
+                let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 1)
+                let forever = SCNAction.repeatForever(rotation)
+                result.node.runAction(forever)
+                // We "let go"
+            } else if sender.state == .ended {
+                result.node.removeAllActions()
+            }
+        }
+    }
+    
+    // Make sure that our objects rotate ournd their center of origin
+    // not their "pivot point"
+    func centerPivot(for node: SCNNode) {
+        let min = node.boundingBox.min
+        let max = node.boundingBox.max
+        node.pivot = SCNMatrix4MakeTranslation(
+            min.x + (max.x - min.x)/2,
+            min.y + (max.y - min.y)/2,
+            min.z + (max.z - min.z)/2
+        )
+    }
+    
     // Our add item function
     func addItem(hitTestResult: ARHitTestResult) {
         if let selectedItem = self.selectedItem {
+            print(selectedItem)
             let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
             // If recursively is true, it checks the entire subtree of the root node
             let node  = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
@@ -75,6 +146,10 @@ class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollec
             let thirdColumn = transform.columns.3
             // Position our item exactly where the plane was detected
             node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            // Fix pivot point issues for certain items
+            if selectedItem == "table" {
+                self.centerPivot(for: node)
+            }
             // Add the new child item
             self.ikeaSceneView.scene.rootNode.addChildNode(node)
         }
@@ -105,5 +180,20 @@ class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollec
         cell?.backgroundColor = UIColor.orange
     }
     
+    // Display our "plane detected" label
+    // This function gets called whenever an anchor is added to the scene view
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // A horizontal surface was detected
+        DispatchQueue.main.async {
+            guard anchor is ARPlaneAnchor else { return }
+            self.planeDetected.isHidden = false
+            
+            // Hide the label after a few seconds
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.planeDetected.isHidden = true
+            }
+        }
+    }
 
 }
+
